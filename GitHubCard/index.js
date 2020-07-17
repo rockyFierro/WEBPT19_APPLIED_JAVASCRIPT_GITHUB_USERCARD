307f25ede6868f67e7b45fe28a0746ddfc29799d
rockyFierro <finnslighthouse@gmail.com>
2020-07-17 13:03:22 -0700

    initial commit has turned into finished with one stretch goal (programmatically adding other cards) ; if more time allows tomorrow may add some expanding info and try to fill it with information from another api.

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,60 +1,79 @@
-/*
-  STEP 1: using axios, send a GET request to the following URL
-    (replacing the placeholder with your Github name):
-    https://api.github.com/users/<your name>
-*/
+import axios from 'axios';
 
-/*
-  STEP 2: Inspect and study the data coming back, this is YOUR
-    github info! You will need to understand the structure of this
-    data in order to use it to build your component function
+const myGit = `http://api.github.com/users/rockyFierro`;
+const followGit = 'http://api.github.com/users/rockyFierro/following';
 
-    Skip to STEP 3.
-*/
+function makeEl(element) {
+    return document.createElement(element);
+}
 
-/*
-  STEP 4: Pass the data received from Github into your function,
-    and append the returned markup to the DOM as a child of .cards
-*/
+const cardMaker = (obj) => {
+    const card = makeEl('div');
+    const proImg = makeEl('img');
+    const cardInfo = makeEl('div');
+    const cardName = makeEl('h3');
+    const cardUName = makeEl('p');
+    const cardLocation = makeEl('p');
+    const cardProfile = makeEl('p');
+    const cardProfileUrl = makeEl('a');
+    const cardFollowers = makeEl('p');
+    const cardFollowing = makeEl('p');
+    const cardBio = makeEl('p');
 
-/*
-  STEP 5: Now that you have your own card getting added to the DOM, either
-    follow this link in your browser https://api.github.com/users/<Your github name>/followers,
-    manually find some other users' github handles, or use the list found at the
-    bottom of the page. Get at least 5 different Github usernames and add them as
-    Individual strings to the friendsArray below.
+    cardInfo.classList.add('class-info');
+    card.classList.add('card');
+    cardName.classList.add('name');
+    cardUName.classList.add('username');
+    cardName.textContent = obj.name;
+    proImg.src = obj.avatar_url;
+    cardUName.textContent = obj.login;
+    cardLocation.text = `location: ${obj.location}`;
+    cardProfile.textContent = "Profile: ";
+    cardProfileUrl.src = obj.html_url;
+    cardProfileUrl.textContent = obj.html_url;
+    cardFollowers.textContent = `Followers: ${obj.followers}`;
+    cardFollowing.textContent = `Following: ${obj.following}`;
 
-    Using that array, iterate over it, requesting data for each user, creating a new card for each
-    user, and adding that card to the DOM.
-*/
+    card.appendChild(proImg);
+    card.appendChild(cardInfo);
+    cardProfile.appendChild(cardProfileUrl);
+    cardInfo.append(cardName,
+        cardUName,
+        cardLocation,
+        cardProfile,
+        cardFollowers,
+        cardFollowing);
 
-const followersArray = [];
+    return card;
+}
 
-/*
-  STEP 3: Create a function that accepts a single object as its only argument.
-    Using DOM methods and properties, create and return the following markup:
+function getCard(){
+    axios
+        .get(myGit)
+        .then( response => {
+            document.getElementsByClassName('cards')[0].appendChild(cardMaker(response.data));
+        })
+}
 
-    <div class="card">
-      <img src={image url of user} />
-      <div class="card-info">
-        <h3 class="name">{users name}</h3>
-        <p class="username">{users user name}</p>
-        <p>Location: {users location}</p>
-        <p>Profile:
-          <a href={address to users github page}>{address to users github page}</a>
-        </p>
-        <p>Followers: {users followers count}</p>
-        <p>Following: {users following count}</p>
-        <p>Bio: {users bio}</p>
-      </div>
-    </div>
-*/
+function getFollowers(){
+    axios
+        .get(followGit)
+        .then( response => {
+            response.data.forEach(
+                el => {
+                    axios
+                        .get(el.url)
+                        .then(
+                            response => {
+                                document.getElementsByClassName('cards')[0].appendChild(cardMaker(response.data));
+                            }
+                        )
+                }
+            );
+        })
+}
 
-/*
-  List of LS Instructors Github username's:
-    tetondan
-    dustinmyers
-    justsml
-    luishrd
-    bigknell
-*/
+getCard();
+getFollowers();
+
+
